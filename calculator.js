1bd52727f73b334ce16d0e29ebd490ecc526794c
Hyunsujk <hyunsujk@gmail.com>
2020-03-13 16:10:48 -0500

    submit jQuery available

--- calculator.js
+++ calculator.js
@@ -3,4 +3,10 @@ $(document).ready(init);
 
 function init() {
   console.log("init");
+  $(".js-add-employees").on("submit", submitEmployeeInfo);
+}
+
+function submitEmployeeInfo(event) {
+  event.preventDefault();
+  console.log("submit");
 }
