4b23fd7311f9dc993f4cfa47c1e26484c944cff2
Hyunsujk <hyunsujk@gmail.com>
2020-03-13 16:25:26 -0500

    render works

--- calculator.js
+++ calculator.js
@@ -1,4 +1,7 @@
 console.log("calculator");
+
+const employees = [];
+
 $(document).ready(init);
 
 function init() {
@@ -9,4 +12,20 @@ function init() {
 function submitEmployeeInfo(event) {
   event.preventDefault();
   console.log("submit");
+
+  const employee = {
+    firstName: $(".js-input-firstName").val(),
+    lastName: $(".js-input-lastName").val(),
+    id: $(".js-input-id").val(),
+    title: $(".js-input-title").val(),
+    annualSalary: $(".js-input-annualSalary").val()
+  };
+
+  employees.push(employee);
+  console.log(employees);
+  render();
+}
+
+function render() {
+  console.log("render");
 }
