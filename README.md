3dddf44769fb84ff57fc57007dd6a9e15846f8be
Hyunsujk <hyunsujk@gmail.com>
2020-03-15 14:58:35 -0500

    updated readme

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 Duration: 2 days / weekend challenge
 
 This application allows users to add their employees and get an updated total monthly cost.
+Built with JavaScript and jQuery (selectors, append and event handling)
+
+- every line of code has explanation of what it's doing
 
 [Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
 
@@ -15,10 +18,12 @@ This application allows users to add their employees and get an updated total mo
 ---
 
 - Input form collects employee's first name, last name, ID number, job title and annual salary.
-- When user clicks submit button, the added employee's information will be shown in the table below and total monthly cost will be updated.
+- When user clicks submit button, the employee's information will be added to the table below and total monthly cost will be updated.
 - User can use delete button to remove the employee from the list. Total monthly cost will be updated accordingly.
 - when the total monthly cost exceeds \$20,000, background color for the total monthly cost will change to red.
 
 ## Screenshot
 
-![ScreenShot](jQuery salary calculator screenshot.png)
+---
+
+![ScreenShot](jQuery-salary-calculator-screenshot.png)
--- calculator.js
+++ calculator.js
@@ -2,9 +2,10 @@ console.log("calculator");
 
 const employees = [];
 let totalMonthly = 0;
-
+// once the document is ready, function init will execute
 $(document).ready(init);
 
+// init function will add functionality to submit button and delete button
 function init() {
   console.log("init");
   $(".js-add-employees").on("submit", submitEmployeeInfo);
@@ -14,20 +15,25 @@ function init() {
 function deleteEmployee(event) {
   //   console.log("event:", event);
   //   console.log("this:", this);
+  // store the index of deleted item
   const deleteThis = $(this).data("index");
   // console.log("deleteThis:", deleteThis);
   // console.log(employees[deleteThis]);
   // const amtDeducted = employees[deleteThis].annualSalary;
   // console.log("amtDeducted", amtDeducted);
+  // removed the item with the index from the array
   employees.splice(deleteThis, 1);
   // console.log(employees);
+  // update the DOM with updated array
   render();
 }
 
 function submitEmployeeInfo(event) {
+  // prevent the submit event to refresh the page
   event.preventDefault();
   //   console.log("submit");
 
+  // store the employee's information from the input field to a variable called 'employee'
   const employee = {
     firstName: $(".js-input-firstName").val(),
     lastName: $(".js-input-lastName").val(),
@@ -35,11 +41,12 @@ function submitEmployeeInfo(event) {
     title: $(".js-input-title").val(),
     annualSalary: $(".js-input-annualSalary").val()
   };
-
+  // add the employee to employees array
   employees.push(employee);
   //   console.log('employees:',employees);
+  // update the DOM with updated array
   render();
-
+  // empty the input field
   $(".js-input-firstName").val("");
   $(".js-input-lastName").val("");
   $(".js-input-id").val("");
@@ -49,16 +56,25 @@ function submitEmployeeInfo(event) {
 
 function render() {
   //   console.log("render");
+  // empty the table to start from scratch
   $(".js-table-body").empty();
+  // empty the total monthly to start from scratch
   $(".js-total-monthly").empty();
+  // start the total monthly at 0
   let totalMonthly = 0;
+  // start the employee monthly at 0
   let employeeMonthly = 0;
+  /* loop through the employees array. 
+  For every single index, divide annual salary by 12 and store it to employee monthly.
+  add employee monthly to total monthly for the updated amount
+  */
   for (let i = 0; i < employees.length; i++) {
     const individual = employees[i];
     employeeMonthly = parseFloat(individual.annualSalary / 12);
     // console.log(employeeMonthly);
     totalMonthly += employeeMonthly;
     // console.log(("individual:", individual));
+    // append the employee's information to the table with delete button
     $(".js-table-body").append(`<tr>
     <td>${individual.firstName}</td>
     <td>${individual.lastName}</td>
@@ -68,8 +84,11 @@ function render() {
     <td><button class="alnCenter btn js-btn-delete" data-index="${i}">Delete</button></td>
     </tr>`);
   }
+  // update total monthly cost
   $(".js-total-monthly").text(totalMonthly);
-
+  /* if total monthly is bigger than $20,000, background color will change to red
+    if not, it will be white.
+  */
   if (totalMonthly > 20000) {
     $(".final").css("background-color", "red");
   } else {
